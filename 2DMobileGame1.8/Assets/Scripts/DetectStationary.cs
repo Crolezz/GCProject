63f49fadff29fda9e2f7e4b328cad53d2221f309
Crolezz <56538741+Crolezz@users.noreply.github.com>
2019-10-16 17:39:11 +0100

    new

--- 2DMobileGame1.8/Assets/Scripts/DetectStationary.cs
+++ 2DMobileGame1.8/Assets/Scripts/DetectStationary.cs
@@ -4,30 +4,24 @@ using UnityEngine;
 
 public class DetectStationary : MonoBehaviour
 {
-    private float timeToStationary = 2f;
-    private bool A = false;
+    private float timeToStationary = 1f;
     public bool isStationary = false;
 
-    void Start()
-    {
-        
-    }
-
-    void OnCollisionEnter2D(Collision2D other)
+    void OnCollisionStay2D(Collision2D other)
     {
         if (other.gameObject.tag == "Platform" || other.gameObject.tag == "Stackables")
-        {
-            A = true;
+        {      
+            timeToStationary -= Time.deltaTime;
+
+            /*foreach (ContactPoint2D contact in other.contacts)
+            {
+                Debug.DrawRay(contact.point, contact.normal * 10, Color.white);
+            }*/
         }
-        
     }
 
     void Update()
-    {
-        if (A == true)
-        {
-            timeToStationary -= Time.deltaTime;
-        }
+    { 
 
         if (timeToStationary <= 0)
         {
@@ -36,7 +30,7 @@ public class DetectStationary : MonoBehaviour
             timeToStationary = 1000;
         }
 
-        //Debug.Log(timeToStationary);
+        Debug.Log(timeToStationary);
     }
 
 }
