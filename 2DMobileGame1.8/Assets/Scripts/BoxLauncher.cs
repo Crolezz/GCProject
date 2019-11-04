0818f4fb08cfb6a4d0b5e7c550a6757eb86f1a04
Crolezz <56538741+Crolezz@users.noreply.github.com>
2019-11-04 19:51:35 +0000

    Small

--- 2DMobileGame1.8/Assets/Scripts/BoxLauncher.cs
+++ 2DMobileGame1.8/Assets/Scripts/BoxLauncher.cs
@@ -11,7 +11,7 @@ public class BoxLauncher : MonoBehaviour
     public static float nextFire = 1f;
 
     private float startGameTimer = 0f;
-    public static float nextStartGameTimer = 10f;  //set the seconds that you want to wait before the game starts
+    public static float nextStartGameTimer = 14f;  //set the seconds that you want to wait before the game starts
     private bool canStartGame = false;             //the amount of seconds is nextStartGameTimer / 2
     public static bool showTutorialOnce = false;   
 
