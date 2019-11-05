e0f44f58cee9e0b54e6fa0e1b867434240d06d73
Crolezz <56538741+Crolezz@users.noreply.github.com>
2019-11-05 12:38:20 +0000

    TNT block added + functionality

--- 2DMobileGame1.8/Assets/Scripts/box1TNTController.cs
+++ 2DMobileGame1.8/Assets/Scripts/box1TNTController.cs
@@ -10,6 +10,9 @@ public class box1TNTController : MonoBehaviour
     private float nextExplo = 0f;
     private float nextExploCounter = 5;
 
+    public float a = 0;
+    public float b = 0;
+
     void Start()
     {
         exploText = gameObject.GetComponentInChildren<Text>();
@@ -18,20 +21,38 @@ public class box1TNTController : MonoBehaviour
     
     void Update()
     {
-        if (nextExploCounter > 0)
+        //shows the timer to explode in seconds (displayed on the box)
+        if (nextExploCounter >= 0 && DetectStationary.isStationary == false)
         {
             nextExploCounter -= Time.deltaTime;
 
             exploText.text = "" + Mathf.RoundToInt(nextExploCounter);
         }
-        else
+        else if (nextExploCounter <= 0)
         {
             Destroy(gameObject);
         }
-
-        if (nextExploCounter <= 3.5f)
+        if (DetectStationary.isStationary == true)
         {
-            exploText.fontSize = 120;
+            Destroy(exploText);
         }
+
+        //makes the text expand and retract after each second has passed 
+        //adds feeling of urgency
+        for (int i = 5; i >= 0; i--)
+        {
+            if (nextExploCounter <= i + 0.5f && nextExploCounter >= i + 0.35f)
+            {
+                a += Time.deltaTime / 0.15f;
+                exploText.rectTransform.localScale = new Vector3(Mathf.Lerp(0.004f, 0.006f, a), Mathf.Lerp(0.004f, 0.006f, a), Mathf.Lerp(0.004f, 0.006f, a));   
+            }
+            if (nextExploCounter <= i + 0.35f && nextExploCounter >= i + 0.10f)
+            {
+                b += Time.deltaTime / 0.15f;
+                exploText.rectTransform.localScale = new Vector3(Mathf.Lerp(0.006f, 0.004f, b), Mathf.Lerp(0.006f, 0.004f, b), Mathf.Lerp(0.006f, 0.004f, b));
+            }
+        }
+     
+        Debug.Log(a);
     }
 }
