e0e07c2e9959234ccf6c862b750dd183800ca886
Crolezz <56538741+Crolezz@users.noreply.github.com>
2019-12-09 11:09:06 +0000

    Added Endless Mode(still need to change values)

---
+++ 2DMobileGame1.8/Assets/Scripts/DontDestroyScripts/EndlessMode.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessMode : MonoBehaviour
+{
+    public GameObject DDOLEndlessMode;
+    public static bool thisMode = false;
+
+    void Update()
+    {
+        DontDestroyOnLoad(DDOLEndlessMode);
+    }
+
+    public void ToggleEndless()
+    {
+        if (thisMode == false)
+        {
+            thisMode = true;
+        }
+        else if (thisMode == true)
+        {
+            thisMode = false;
+        }
+        Debug.Log(thisMode);
+    }
+}
